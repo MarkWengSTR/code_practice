6cf857d86c99d88f52b7d384ffe3814341a40822
Mark <bskin0330@gmail.com>
2021-03-06 10:28:33 +0800

    Add mock example

--- serious_python.py
+++ serious_python.py
@@ -1,29 +1,56 @@
+from unittest import mock
+
 import pytest
+import requests
 
 
-class Car(object):
-    def __init__(self, name):
-        self.name = name
+class WhereIsPythonError(Exception):
+    pass
 
 
-c1 = Car("Tesla")
+def is_python_still_a_programming_language():
+    try:
+        r = requests.get("http://python.org")
+    except IOError:
+        pass
+    else:
+        if r.status_code == 200:
+            return "Python is a programming language" in r.content
+    raise WhereIsPythonError("Something bas happened")
 
 
-@pytest.fixture
-def car():
-    return Car("Tesla")
+def get_fake_get(status_code, content):
+    m = mock.Mock()
+    m.status_code = status_code
+    m.content = content
+
+    def fake_get(url):
+        return m
+
+    return fake_get
 
 
-def test_car_name(car):
-    assert car.name == "Tesla"
+def raise_get(url):
+    raise IOError("Unable to fetch url %s" % url)
 
 
-def test_car_name2(car):
-    assert car.name == "esla"
+@mock.patch('requests.get', get_fake_get(200, "Python is a programming language for sure"))
+def test_python_is():
+    assert is_python_still_a_programming_language() is True
 
-    # def test_true():
-    #     assert True
 
-    # @pytest.mark.skipif(2 == 1, reason="skip this test")
-    # def test_false():
-    #     assert False
+@mock.patch('requests.get', get_fake_get(200, "Python is no more a programming language for sure"))
+def test_python_is_not():
+    assert is_python_still_a_programming_language() is False
+
+
+@mock.patch('requests.get', get_fake_get(404, "Whatever"))
+def test_bad_status_code():
+    with pytest.raises(WhereIsPythonError):
+        is_python_still_a_programming_language()
+
+
+@mock.patch('requests.get', raise_get)
+def test_ioerror():
+    with pytest.raises(WhereIsPythonError):
+        is_python_still_a_programming_language()
